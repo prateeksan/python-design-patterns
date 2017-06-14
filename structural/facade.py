baaffae1946de74fef89c56e8d69d1a58da395b8
prateeksan <sanyal.prateek@gmail.com>
2017-06-13 20:35:30 -0400

    added notes docstring

---
+++ structural/facade.py
@@ -0,0 +1,14 @@
+""" The Facade Pattern
+
+Notes:
+
+In my opinion, this is one of the most intuitive design patterns that I have
+come across. The point of the Facade pattern is to create a high-level unified
+interface that hides more complex objects behind it. It is particularly useful
+in situations where:
+
+> There is need for a system which relies on several sub-systems or sub-objects.
+> The user of this system needs to a see a simple interface.
+> The system can be represented as an object.
+
+"""
