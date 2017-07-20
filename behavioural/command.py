7ba75fffa5261ef9f5b886fefa0976d81505c035
prateeksan <sanyal.prateek@gmail.com>
2017-07-19 20:51:52 -0400

    command pattern complete with docs

--- behavioural/command.py
+++ behavioural/command.py
@@ -2,10 +2,24 @@
 
 Notes:
 
+The command pattern encapsulates a series of commands into an object (the
+Invoker). This allows the user to execute the commands in a preset order at a
+later time. It also allows the Invoker to encapsulate additional behaviour (such
+as the ability to handle a command's failure).
+
+This example simulates a database migration system wherein a series of migration
+commands need to be run atomically (all go through or none do) and in a specific
+order. In this case, each command is encapsulated as an instance of
+MigrationCommand (or its child), with the ability to run or rollback the
+migration. The invoker is responsible for maintaining the sequence of migrations
+and their atomicity by rolling back all completed migrations in case of a
+failure. The invoker thus creates and handles a loose coupling of the 
+migration commands.
 
 """
 
 class MigrationCommand:
+    """Represents a migration command which the invoker will call."""
 
     def __init__(self, title, instructions):
         self.title = title
@@ -18,17 +32,23 @@ class MigrationCommand:
         print("[i] Rolling Back Migration: {}".format(self.title))
 
 class BadMigrationCommand(MigrationCommand):
+    """Created for demonstrative purposes only (to test a failure)."""
 
     def run(self): raise RuntimeError("Something went wrong.")
 
 class Invoker:
+    """Responsible for maintaining the sequence and atomicity of loosely coupled
+    migration commands."""
 
     def __init__(self, migrations):
 
         self.migrations = migrations
+        # Tracking the version allows the invoker to perform atomic rollbacks.
         self._version = 0
 
     def run_all(self):
+        """Runs all migrations. If any fail, rolls back all migrations."""
+
         for migration in self.migrations:
             try:
                 migration.run()
