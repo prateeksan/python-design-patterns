790eaf2a11ed33ad9938f3f843a801a417f4f921
prateeksan <sanyal.prateek@gmail.com>
2017-07-23 16:19:34 -0400

    all docstrings complete for memento

--- behavioural/memento.py
+++ behavioural/memento.py
@@ -33,6 +33,9 @@ other relevant meta data (a checkpoint message).
 import copy
 
 class MapCheckpoint:
+    """Acts as the Memento and stores a snapshot state of the map along with
+    a message describing the checkpoint.
+    """
 
     def __init__(self, map_state, message):
 
@@ -40,12 +43,20 @@ class MapCheckpoint:
         self.message = message
 
 class MapBuilder:
+    """Acts as the Originator and is responsible for creating, drawing and
+    maintaining the map. It is also responsible for creating checkpoints at
+    relevant intervals which are stored in an external log. The state of the
+    map can then be reverted to the stored checkpoints at any point.
+    """
 
     def __init__(self, map_size):
+        """The map can be constructed with a variable size."""
+
         self.map_size = map_size
         self.map = self._build_default_map()
 
     def draw(self, coords, value):
+        """Called by the user to add a new value to a tile."""
 
         if not (coords[0] < self.map_size[0] and coords[1] < self.map_size[1]):
             print("Coordinates not in range. Try again!")
@@ -56,30 +67,51 @@ class MapBuilder:
         self.map[coords[1]][coords[0]] = value
 
     def create_checkpoint(self, message):
+        """Creates and returns a snapshot memento of the map's state as a 
+        MapCheckpoint object.
+        """
 
         print("Creating new checkpoint: <{}>".format(message))
         return MapCheckpoint(map_state=copy.deepcopy(self.map), message=message)
 
     def restore_from_checkpoint(self, checkpoint):
+        """Restores the state of a map from a MapCheckpoint. Note how the
+        MapBuilder has no knowledge of logged checkpoints.
+        """
+
         print("Restoring to checkpoint: <{}>".format(checkpoint.message))
         self.map = copy.deepcopy(checkpoint.map_state)
 
     def _build_default_map(self):
+        """Builds a 2d map of the given size with '-' default values."""
+
         return [["-" for x in range(self.map_size[0])] 
             for y in range(self.map_size[1])]
 
     def print_map(self):
+
         for row in self.map:
             print(row)
         print("\n")
 
 class CheckpointLog():
+    """Acts as the caretaker and is responsible for maintaining a history of the
+    checkpoints (mementos) created. It can also load and return a previous
+    checkpoint. One instance of this class should be used to record checkpoints
+    for one instance of the MapBuilder.
+    """
 
     def __init__(self, max_length=3):
+        """The user may specify a max_length for the log which defaults to 3.
+        This represents the max checkpoints that can be recorded for one map
+        builder.
+        """
+
         self._checkpoints = []
         self.max_length = max_length
 
     def add(self, new_checkpoint):
+        """Adds a new MapCheckpoint instance and deletes the oldest one."""
 
         if len(self._checkpoints) == self.max_length:
             del self._checkpoints[0]
@@ -87,6 +119,11 @@ class CheckpointLog():
         self._checkpoints.append(new_checkpoint)
 
     def revert(self, revert_count=1):
+        """Returns an older MapCheckpoint. In a real use-case, this method may
+        also be responsible for re-arranging or deleting checkpoints posterior
+        to the reverted.
+        """
+
         return self._checkpoints[-revert_count]
 
 
@@ -108,4 +145,4 @@ if __name__ == "__main__":
     map_builder.print_map()
 
     map_builder.restore_from_checkpoint(cp_log.revert(3))
-    map_builder.print_map()
+    map_builder.print_map() # The ouput should be the same as line 137
